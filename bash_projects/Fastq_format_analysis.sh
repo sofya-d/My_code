e52eb87fa15537fcda268abc0f1e47308ef4d9fe
Sofya Dubovskova <er.dom@mail.ru>
2022-05-02 11:39:48 +0300

    Update and rename Fastq_format_analysis.md to Fastq_format_analysis.sh

--- bash_projects/Fastq_format_analysis.sh
+++ bash_projects/Fastq_format_analysis.sh
@@ -1,8 +1,5 @@
 # Writes number of lines of txt-like files with exact length and length of those lines in .txt file.
-```console
 for i in *.fq; do echo $i; echo $i >> reads_length.txt; awk '{++a[length()]} END{for (i in a) print i, a[i]}' $i >> reads_length.txt; done
-```
+
 # Writes number of lines and number of reads of .fastq file in .txt file.
-```console
 for i in *.fq; do echo $i; echo $i >> lines_reads.txt; echo "number of lines" >> lines_reads.txt; wc -l < $i >> lines_reads.txt; echo "number of lines divided by 4" >> lines_reads.txt; printf %.2f\\n "$((100 * $(wc -l < $i) / 4))e-2" >> lines_reads.txt; echo "number of reads" >> lines_reads.txt; grep '^@HISEQ' $i | wc -l >> lines_reads.txt; done
-```
